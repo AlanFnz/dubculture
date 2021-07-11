d070a4be1afb00840d022a0b1100521c67713bdd
Alan Fernandez <alan.f@msn.com>
2021-07-11 20:22:02 -0300

    renamed file and added dubfree data

---
+++ client/src/data/data.js
@@ -0,0 +1,41 @@
+export const links = [
+  {
+    id: 1,
+    title: "Download DUBFREE001",
+    url: "https://#",
+  },
+  {
+    id: 2,
+    title: "Bandcamp",
+    url: "https://#",
+  },
+  {
+    id: 3,
+    title: "Soundcloud",
+    url: "https://#",
+  },
+  {
+    id: 4,
+    title: "Instagram",
+    url: "https://#",
+  },
+  {
+    id: 5,
+    title: "Facebook",
+    url: "https://#",
+  },
+  {
+    id: 6,
+    title: "Contact",
+    url: "https://#",
+  },
+];
+
+export const dubfree = [
+  {
+    id: 1,
+    title: "DUBFREE001",
+    url: "https://#",
+    img: "https://#",
+  },
+];
